4347d918474fef8149117de6e187a29c87fa64a6
“bryson” <brysonward77@gmail.com>
2022-06-21 16:54:32 -0400

    made input and search responsive + ratio on imgs

--- src/routes/[searched]/[songId].svelte
+++ src/routes/[searched]/[songId].svelte
@@ -28,7 +28,7 @@
   <section>
     <div class="mt-12 flex flex-col items-center justify-center">
       <h1 class="text-3xl font-bold text-center mb-12">{songResults?.trackName}</h1>
-      <img  src={songResults?.artworkUrl100} alt="img" class="w-1/4 rounded-md mb-12">
+      <img width={100} height={100} src={songResults?.artworkUrl100} alt="img" class=" mb-12">
       <audio controls>
         <source src={songResults?.previewUrl} type="audio/mpeg">
       </audio>
